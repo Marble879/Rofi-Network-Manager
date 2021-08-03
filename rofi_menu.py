ddf556db5d193d3220b9e7bd7df9d97467177185
Marble879 <younisakel879@gmail.com>
2021-08-03 16:35:27 +0200

    #1 Cleanup code

--- rofi_menu.py
+++ rofi_menu.py
@@ -1,18 +1,6 @@
 from rofi import Rofi
 import os
-from NetworkManager import *
 import subprocess
-
-#you get the individual lines by .split("\n")
-#for the next part there are several options
-#they're pretty neatly aligned so you could try to get the string indices of the header row using .find
-#and then read from that index for every line
-#another option is to use a regular expression
-#but maybe you can also split by two spaces (not sure if that always works)
-
-
-#TODO: move this into a method
-rofi = Rofi()
 
 def get_wifi_networks():
     wifi_devices_raw = subprocess.check_output(['nmcli', 'device', 'wifi', 'list'])
@@ -30,8 +18,4 @@ def quick_options_test():
     return index, key
 
 if __name__ == '__main__':
-    # index, key = quickOptionsTest()
-    # print("index: " + str(index) + "\n" + "key: " + str(key))
-    #get_wifi_networks()
-    #os.system("nmcli device wifi list")
     get_wifi_networks()
