ac0ac47857b1ff250e33262a83de10ace2124ee5
Marble879 <younisakel879@gmail.com>
2021-08-03 16:34:26 +0200

    #1 Add ability to detect wifi networks and return a list of them

--- rofi_menu.py
+++ rofi_menu.py
@@ -1,12 +1,37 @@
 from rofi import Rofi
+import os
+from NetworkManager import *
+import subprocess
 
+#you get the individual lines by .split("\n")
+#for the next part there are several options
+#they're pretty neatly aligned so you could try to get the string indices of the header row using .find
+#and then read from that index for every line
+#another option is to use a regular expression
+#but maybe you can also split by two spaces (not sure if that always works)
+
+
+#TODO: move this into a method
 rofi = Rofi()
 
-def quickOptionsTest():
+def get_wifi_networks():
+    wifi_devices_raw = subprocess.check_output(['nmcli', 'device', 'wifi', 'list'])
+    wifi_devices_decoded = wifi_devices_raw.decode("utf-8")
+    wifi_list = wifi_devices_decoded.split('\n')
+    return wifi_list
+
+
+
+
+
+def quick_options_test():
     options = ['Red', 'Green', 'Blue', 'White', 'Silver', 'Black', 'Other']
     index, key = rofi.select('What colour car do you drive?', options)
     return index, key
 
 if __name__ == '__main__':
-    index, key = quickOptionsTest()
-    print("index: " + str(index) + "\n" + "key: " + str(key))
+    # index, key = quickOptionsTest()
+    # print("index: " + str(index) + "\n" + "key: " + str(key))
+    #get_wifi_networks()
+    #os.system("nmcli device wifi list")
+    get_wifi_networks()
---
+++ venv/lib/python3.9/site-packages/nmcli/__init__.py
@@ -0,0 +1,1 @@
+from nmcli import *
